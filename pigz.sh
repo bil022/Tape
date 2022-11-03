0aa02ce0493cb3c321c3c10c21a43aa548d62c01
bil022 <bil022@ucsd.edu>
2022-11-03 13:25:24 -0700

    Update pigz.sh

--- pigz.sh
+++ pigz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for d in $*; do
-  for f in `find $d -ls -type f | sort -nrk7 | grep -v bam$ | grep -v gz$ | grep -v bz2$ | grep -v bw$ | awk '{print $NF}' | head -n 100`; do
+  for f in `find $d -type f -exec ls -l {} + | sort -nrk7 | grep -v bam$ | grep -v gz$ | grep -v bz2$ | grep -v bw$ | awk '{print $NF}' | head -n 100`; do
     file=`./sfile -b $f`
     if [[ $file == *"text"* ]]; then
       echo "It's ASC."
