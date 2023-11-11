f01faf53c9e0e5cc79edd1d80146ecabb1be3342
bil022 <bil022@ucsd.edu>
2023-11-11 15:47:06 -0800

    check_ret

--- fastq_tar.pl
+++ fastq_tar.pl
@@ -5,6 +5,15 @@ $dst=$ARGV[0];
 $dst=~s/\/$//;
 die "dst? $dst" unless ( -d "$dst" );
 
+while (<DATA>) {
+  if (/^(\S+):/) {
+    $SRC{$tag}=$script; undef $script;
+    $tag=$1; next;
+  }
+  $script.=$_;
+}
+$SRC{$tag}=$script if $tag;
+
 while (<STDIN>) {
   chomp();
   s/\/+/\//g;
@@ -22,7 +31,41 @@ while (<STDIN>) {
   push(@{$hash{$key}}, '"'.$path.'"');
 }
 
+print "$SRC{HEAD}" if exists $SRC{HEAD};
 foreach $key (keys %hash) {
   print("echo $key $SIZE{$key} K\n");
   print("tar -cvf $dst/$key.tar -C $dir @{$hash{$key}}\n");
+  print("check_ret $dst/$key.tar \$*\n");
 }
+print "$SRC{TAIL}" if exists $SRC{TAIL};
+
+__END__
+HEAD:
+#!/bin/bash
+  
+df=`df -g . | awk '{n=$4}END{print n}'`
+if [ $df -eq 0 ]; then echo "No space"; exit; fi
+
+pid=${0%%sh}pid
+if [ -f $pid ]; then echo "Found $pid"; exit; fi
+echo $$ > $pid
+
+panic=${0%%sh}panic
+if [ -e $panic ]; then echo "Fanic $panic"; exit; fi
+
+function check_ret {
+  msg=$1; shift
+  for ret in $*; do
+    if [ $ret -ne 0 ]; then
+      echo $msg $* > $panic
+      locate mail
+      if [ $? -eq 0 ]; then
+        echo $msg $* | mail -s "Error in $msg" bil022@ucsd.edu
+      fi
+      exit
+    fi
+  done
+}
+
+TAIL:
+rm $pid
--- fastq_tar.sh
+++ fastq_tar.sh
@@ -8,6 +8,13 @@ shift
 pl=${0%%sh}pl
 if ! [ -e $pl ]; then echo "$pl?"; exit; fi
 
+du=`du -g $* | awk '{print $1}'`
+df=`df -g $* | awk '{n=$4}END{print n}'`
+
+if [ $du -gt $df ]; then
+  echo "Not enough space $du > $df"; exit;
+fi
+
 for dir in $*; do
   if ! [ -d $dir ]; then echo "dir? $dir"; exit; fi
   find $dir -mindepth 1 -maxdepth 1 -exec du -sk '{}' \; | $pl $dst
