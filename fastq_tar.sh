0997ac04a06927525de6347a897a56bb0ba992eb
bil022 <bil022@ucsd.edu>
2023-11-11 22:34:47 -0800

    tape_finder

--- fastq_tar.sh
+++ fastq_tar.sh
@@ -9,11 +9,16 @@ pl=${0%%sh}pl
 if ! [ -e $pl ]; then echo "$pl?"; exit; fi
 
 du=`du -g $* | awk '{print $1}'`
-df=`df -g $* | awk '{n=$4}END{print n}'`
+df=`df -g . | awk '{n=$4}END{print n+1}'`
 
 if [ $du -gt $df ]; then
   echo "Not enough space $du > $df"; exit;
 fi
+
+[ -e tape_finder ] || git clone https://github.com/bil022/tape_finder
+echo "find $* -ls | gzip > tape_finder/$dst.find.txt.gz"
+echo "rsync -av tape_finder/$dst.find.txt.gz $dst/."
+echo "pushd tape_finder; git add $dst.find.txt.gz; git commit -m '$dst.find.txt.gz'; git push; popd"
 
 for dir in $*; do
   if ! [ -d $dir ]; then echo "dir? $dir"; exit; fi
