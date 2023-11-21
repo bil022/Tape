3f8d05441a5e1d4980ce2761ed2878c164df2f56
bil022 <bil022@ucsd.edu>
2023-11-21 11:40:40 -0800

    tape_finder

--- fastq_tar.sh
+++ fastq_tar.sh
@@ -8,18 +8,18 @@ shift    #move to the next parameter
 pl=${0%%sh}pl    #check if the corresponding .pl file exists
 if ! [ -e $pl ]; then echo "$pl?"; exit; fi    #if not, print out the error and exit
 
-du=`du -k $* | awk '{print $1}'`    ## ??? du -h --human-readable ??? ###the check source folder's size
+du=`du -sk $* | awk '{print $1}'`    ## ??? du -h --human-readable ??? ###the check source folder's size
 df=`df -k . | awk '{n=$4}END{print n+1000}'`    ## ??? df -H ??? ###check the destination space availability
 
 if [ $du -gt $df ]; then    ##if no enough space
   echo "Not enough space $du > $df"; exit;    ##print out the error and exit
 fi
 
-[ -e tape_finder ] || git clone https://github.com/bil022/tape_finder
-bz2=tape_finder/$dst.find.txt.bz2
-echo "find $* -ls | bzip2 > $bz2"
-echo "rsync -av $bz2 $dst/."
-echo "pushd tape_finder; git add $bz2; git commit -m '$bz2'; git push; popd"
+#[ -e tape_finder ] || git clone https://github.com/bil022/tape_finder
+#bz2=tape_finder/$dst.find.txt.bz2
+#echo "find $* -ls | bzip2 > $bz2"
+#echo "rsync -av $bz2 $dst/."
+#echo "pushd tape_finder; git add $bz2; git commit -m '$bz2'; git push; popd"
 
 for dir in $*; do    #for each directory
   if ! [ -d $dir ]; then echo "dir? $dir"; exit; fi    #if the directory does not exist, print out the error and exit
